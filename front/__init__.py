81c725404edec6ba63e745f25f129b1ba0b3cc2f
hcz <h.cz@qq.com>
2013-11-23 00:58:45 +0800

    Add front.datatype

--- front/__init__.py
+++ front/__init__.py
@@ -1,1 +1,1 @@
-# from ??? import *
+from datatype import *
---
+++ front/datatype.py
@@ -0,0 +1,19 @@
+import mavc.datatype as datatype
+
+def Package(comment, *data):
+    return datatype.PackageDataType(comment, set(data))
+
+def Commit(comment, *data):
+    return datatype.CommitDataType(comment, set(data))
+
+def CommitWithTime(comment, time, *data):
+    return datatype.CommitDataType(comment, set(data), time)
+
+def Task(comment, *data):
+    return datatype.TaskDataType(comment, list(data))
+
+def Dir(targetdir, *data):
+    return datatype.DirDataType(targetdir, set(data))
+
+def File(targetfile):
+    return datatype.FileDataType(targetfile)
