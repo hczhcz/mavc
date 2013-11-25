987c7c17ccbc0513e557f330f76d002bbfc8de21
hcz <h.cz@qq.com>
2013-11-26 01:29:50 +0800

    Add datatypeex

--- front/datatype.py
+++ front/datatype.py
@@ -1,34 +1,44 @@
 from mavc import info
 from mavc import datatype
 
+def _Expand(data):
+    '''If data is a list or set in another list or set, expand it'''
+
+    if len(data) == 1 and (
+        isinstance(data[0], set) or isinstance(data[0], list)
+    ):
+        return data[0]
+    else:
+        return data
+
 def Package(comment, *data):
     '''Create package data object'''
 
-    info.LastData = datatype.PackageDataType(comment, set(data))
+    info.LastData = datatype.PackageDataType(comment, set(_Expand(data)))
     return info.LastData
 
 def Commit(comment, *data):
     '''Create commit data object'''
 
-    info.LastData = datatype.CommitDataType(comment, set(data))
+    info.LastData = datatype.CommitDataType(comment, set(_Expand(data)))
     return info.LastData
 
-def CommitWithTime(comment, time, *data):
+def CommitTimed(comment, time, *data):
     '''Create commit data object and set time stamp manually'''
 
-    info.LastData = datatype.CommitDataType(comment, set(data), time)
+    info.LastData = datatype.CommitDataType(comment, set(_Expand(data)), time)
     return info.LastData
 
 def Task(comment, *data):
     '''Create task data object'''
 
-    info.LastData = datatype.TaskDataType(comment, list(data))
+    info.LastData = datatype.TaskDataType(comment, list(_Expand(data)))
     return info.LastData
 
 def Dir(targetdir, *data):
     '''Create dir data object'''
 
-    info.LastData = datatype.DirDataType(targetdir, set(data))
+    info.LastData = datatype.DirDataType(targetdir, set(_Expand(data)))
     return info.LastData
 
 def File(targetfile):
--- front/datatypeex.py
+++ front/datatypeex.py
@@ -1,6 +1,42 @@
+import os
+import re
 from mavc import info
+import datatype
 
 def Last():
     '''Last generated data object'''
 
     return info.LastData
+
+def Walk(target, ignore = r'.*' + os.sep + r'\..*'):
+    '''Walking and create data objects
+    Select path and file using ignore rule (regular expression or function)
+    Return a set'''
+
+    # Compile the ignore rule
+    # Regular expression to lambda
+    if isinstance(ignore, str):
+        ReObject = re.compile(ignore)
+        ignore = lambda x: ReObject.match(x)
+
+    info.Log.Message('Scanning dir ' + target)
+
+    Result = set()
+
+    if not os.path.isdir(target):
+        info.Log.Error('Not a dir ' + target)
+
+    for item in os.listdir(target):
+        NewPath = target + item
+
+        # Checking
+        if not ignore(NewPath):
+            info.Log.Message('Accepted path ' + NewPath)
+            if os.path.isdir(NewPath):
+                # Is dir
+                Result.add(datatype.Dir(item, Walk(NewPath + os.sep, ignore)))
+            elif os.path.isfile(NewPath):
+                # Is file
+                Result.add(datatype.File(item))
+
+    return Result
