34142c9e09115e04dd589186d22a2e80728f91a2
hcz <h.cz@qq.com>
2013-12-16 02:19:00 +0800

    Move main to __main__ module

--- __init__.py
+++ __init__.py
@@ -1,19 +1,9 @@
-if __name__ == '__main__':
-    import sys
-    import os
-    sys.path.append(os.path.abspath('.'))
-    from mavc import *
-    del os
-    del sys
+import info
+import interface
+import datatype
+import module
+import init
+import front
+from front import *
 
-    init.Repl(globals())
-else:
-    import info
-    import interface
-    import datatype
-    import module
-    import init
-    import front
-    from front import *
-
-    init.Init()
+init.Init()
---
+++ __main__.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath('.'))
+from mavc import *
+del os
+del sys
+
+init.Repl(globals())
